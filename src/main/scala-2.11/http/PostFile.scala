3844009e192158ed92fe5214b3dd79eb677f4da3
Yoone <yoann@yoone.eu>
2015-07-10 23:14:36 +0200

    Upload: added a method to generate uploaded files' tmpPath

--- src/main/scala-2.11/http/PostFile.scala
+++ src/main/scala-2.11/http/PostFile.scala
@@ -1,9 +1,27 @@
 package http
 
+import java.io.File
+import java.util.UUID
+
 /**
  * Created by mecavity on 10/07/15.
  */
 class PostFile {
   var tmpPath: String = null
   var fileName: String = null
+
+  def generateTempPath (): Unit = {
+    assert(fileName != null, "Filename should not be null at this point")
+
+    tmpPath = sys.props.get("java.io.tmpdir").get + "/scala-website-uploads/"
+    val dir = new File(tmpPath)
+    if (!dir.exists()) {
+      assert(dir.mkdir(), "Could not create tmp directory")
+    }
+
+    tmpPath += UUID.randomUUID()
+
+    val ext = fileName.split('.')
+    tmpPath += (if (ext.length > 2) "." + ext(ext.length - 1) else "")
+  }
 }
