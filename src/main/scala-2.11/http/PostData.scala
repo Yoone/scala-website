a89529418c3cdf4a1c0128f797987be884fba461
Elliot Chen <elliot.chen7@gmail.com>
2015-07-11 18:35:16 +0200

    Parser: Finally able to save file correctly

--- src/main/scala-2.11/http/PostData.scala
+++ src/main/scala-2.11/http/PostData.scala
@@ -2,54 +2,66 @@ package http
 
 import util.URLHandler
 
-import scala.io.StdIn
+import java.io._
 
 /**
  * Created by mecavity on 10/07/15.
  */
 object PostData {
-  def parseFile(request: Request, dataSource: String, boundary: String): Unit = {
-    var buf: String = ""
-    var data = dataSource
-    while (buf != null && !data.contains("\n\n")) {
-      data += buf + "\n"
-      buf = StdIn.readLine()
-    }
-    val name: String = data.substring(data.indexOf("name=") + 6, data.indexOf("\"", data.indexOf("name=") + 6))
-    var file = new PostFile()
+  val reader = new InputStreamReader(System.in)
+
+  def saveBinaryFile(request: Request, data: String, boundary: String): Unit = {
+    val file = new PostFile()
     file.fileName = data.substring(data.indexOf("filename=") + 10, data.indexOf("\"", data.indexOf("filename=") + 10))
     file.tmpPath = "files/" + file.fileName // TODO generate tmp path
 
+    val output = new DataOutputStream(new FileOutputStream(file.tmpPath))
+    var b: Int = System.in.read()
+    try {
+      while (b != -1) {
+        output.write(b)
+        b = System.in.read()
+      }
+    }
+    finally output.close()
+  }
+
+  def parseFile(request: Request, dataSource: String, boundary: String): Unit = {
+    var b: Int = System.in.read()
+    var data = dataSource
+    while (b != -1 && !data.contains("\r\n\r")) { // TODO check why we need to leave last '\n'
+      data += b.toChar
+      b = System.in.read()
+    }
+    val name: String = data.substring(data.indexOf("name=") + 6, data.indexOf("\"", data.indexOf("name=") + 6))
+
     // Read Binary File
-    URLHandler.getFile(request, boundary)
+    saveBinaryFile(request, data, boundary)
   }
 
   def parseField(request: Request, str: String): Unit = {
     if (str == "--" || str == "--\n")
       return
     val name: String = str.substring(str.indexOf("name=") + 6, str.indexOf("\"", str.indexOf("name=") + 6))
-    val value: String = str.substring(str.indexOf("\n\n") + 2, str.indexOf("--"))
+    val value: String = str.substring(str.indexOf("\r\n\r\n") + 4, str.indexOf("--"))
     request.POST += (URLHandler.decode(name) -> URLHandler.decode(value))
   }
 
-  def multipart(request: Request, boundary: String): Unit = {
-    var buf = StdIn.readLine()
+  def multipart (request: Request, boundary: String): Unit = {
     var data = ""
-    var elt: String = null
-    while (buf != null) {
-      data += buf + "\n"
-      buf = StdIn.readLine()
-      // Check if a byte read will have to be performed
-      if (data.contains("filename")) {
-        parseFile(request, data, boundary) // TODO
-        data = ""
-        return // TODO REMOVE
-      }
-      else if (data.contains(boundary)) {
+    var b: Int = System.in.read()
+    while (b != -1) {
+      data += b.toChar
+      if (data.contains(boundary)) {
         data = data.substring(0, data.indexOf(boundary))
         parseField(request, data)
         data = ""
       }
+      else if (data.contains("filename")) {
+        parseFile(request, data, boundary)
+        data = ""
+      }
+      b = System.in.read()
     }
   }
 }
