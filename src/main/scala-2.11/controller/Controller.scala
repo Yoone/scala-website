64656802c9fb82231d4b6661e1058c2daca51b57
Elliot Chen <elliot.chen7@gmail.com>
2015-07-13 02:33:16 +0200

    HangMan: Creation of a new game + gameplay of ongoing game (adding letter, returning word and status of game)

--- src/main/scala-2.11/controller/Controller.scala
+++ src/main/scala-2.11/controller/Controller.scala
@@ -50,11 +50,54 @@ object Controller {
     val words = TableQuery[Word]
     val games = TableQuery[Game]
 
+    var all_guesses = ""
+    var guess = " "
+
+    // val current_game: Int = request.GET.getOrElse("id", )
+    val id = request.GET.getOrElse("id", 0)
+    val current_game: Int = id match {
+      case x: String => x.toInt
+      case _ => 0
+    }
+
+    request.GET.getOrElse("action", "") match {
+      case "new" => DatabaseHandler.database withSession {
+        implicit session =>
+          val game_id = (games returning games.map(_.id)) +=(0, 1, "")
+          println("{\"game_id\":" + game_id + "}")
+          return ""
+      }
+      case x if current_game == 0 => println("{\"error\":\"Bad game id\"}")
+        return ""
+        // TODO check if current_game exists
+      case x => DatabaseHandler.database withSession {
+        implicit session =>
+          all_guesses = games.filter(_.id === current_game).map(_.guessed).first.toLowerCase
+          all_guesses += (if (all_guesses.contains(x)) "" else x).toLowerCase
+          games.filter(_.id === current_game).map(_.guessed).update(all_guesses)
+          guess = x.toLowerCase
+      }
+    }
+    var word = ""
     DatabaseHandler.database withSession {
       implicit session =>
-        val game_id = (games returning games.map(_.id)) += (0, 1, "")
-        println("{\"game_id\":" + game_id + "}")
+        val game_word_id = games.filter(_.id === current_game).map(_.word_id)
+        word = words.filter(_.id in game_word_id).map(_.word).first.toLowerCase
     }
+
+    var result = ""
+    word.foreach(x => result += ((if (all_guesses.contains(x)) x else '_') + " "))
+    result = result.trim.toUpperCase
+
+    var finished = true
+    word.foreach(x => if (!all_guesses.contains(x)) finished = false)
+
+    // success, finished, word
+    println("{\"word\":" + result + "\",\"success\":" + word.contains(guess) + ",\"finished\":" + finished + "}")
+    //println("Game: " + current_game)
+    //println("Objective: " + word)
+    //println("Guessed: " + all_guesses)
+    //println("Current: " + result)
 
     ""
   }
